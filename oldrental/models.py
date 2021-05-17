3342b41f1cb2ffea0b63d85d1dd124acc1408b23
Kacper Szymański <stefek9113@gmail.com>
2021-05-17 21:38:56 +0200

    genre and book added
    added Genre, Book models and also added few examples to it

--- oldrental/models.py
+++ oldrental/models.py
@@ -1,12 +1,37 @@
 from django.db import models
+from datetime import datetime
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Genre(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Book(models.Model):
+    author = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    genre = models.ForeignKey(Genre, related_name='books', on_delete=models.PROTECT)
+    isbn = models.CharField(unique=True, max_length=13, validators=[
+        RegexValidator(regex='^[0-9]{13}$', message='ISBN must be 13 digit long!', code='no_match')])
+    user_rentier = models.ForeignKey(User, related_name='books', on_delete=models.CASCADE,
+                                     default=None, blank=True, null=True)
+    rent_date = models.DateTimeField(default=None, blank=True, null=True)
+
+    def rent(self, user):
+        self.user_rentier = user
+        self.rent_date = datetime.now()
+
+    def unrent(self):
+        pass
+
+    def get_absolute_url(self):
+        return reverse('books:details', args=[self.pk])
+
+    class Meta:
+        unique_together = ('author', 'title', 'genre')
+        ordering = ('title',)
