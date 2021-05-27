8d36c0a150a5104e957b0431c94db4cbc0c67d0b
Kacper Szymański <stefek9113@gmail.com>
2021-05-27 21:03:25 +0200

    added first view with css

--- oldrental/views.py
+++ oldrental/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Book
+
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +8,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the oldrental index.")
+
+
+def books(request):
+    books = Book.objects.filter().order_by('author')
+    return render(request, 'books.html', {'books': books})
