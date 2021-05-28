7cb19ea0ea371873b3b89a020a890212709781e3
Kacper Szymański <stefek9113@gmail.com>
2021-05-28 19:10:00 +0200

    added edit and add form

--- oldrental/urls.py
+++ oldrental/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.books, name='books'),
     path('book/<int:pk>/', views.books_detail, name='books_detail'),
+    path('book/new', views.book_new, name='book_new'),
+    path('book/<int:pk>/edit/', views.book_edit, name='book_edit'),
 
 ]
--- oldrental/views.py
+++ oldrental/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from django.shortcuts import render, get_object_or_404
-
+from .forms import BookForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -15,6 +15,32 @@ def books(request):
     books = Book.objects.filter().order_by('author')
     return render(request, 'books.html', {'books': books})
 
+
 def books_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'book_detail.html', {'book': book})
+
+
+def book_new(request):
+    if request.method == "POST":
+        book = BookForm(request.POST)
+        if book.is_valid():
+            book_temp = book.save(commit=False)
+            book_temp.save()
+            return redirect('book_detail.html', pk=book_temp.pk)
+    else:
+        book = BookForm()
+    return render(request, 'book_edit.html', {'book': book})
+
+
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book_temp = form.save(commit=False)
+            book_temp.save()
+            return redirect('book_detail.html', pk=book_temp.pk)
+    else:
+        book = BookForm(instance=book)
+    return render(request, 'book_edit.html', {'book': book})
