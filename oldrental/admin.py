a47881392ae4fcde6a4ed2e00a48ecdfe0d9e21f
Kacper Szymański <stefek9113@gmail.com>
2021-06-03 20:40:49 +0200

    added 1:1 film

--- oldrental/admin.py
+++ oldrental/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
-<<<<<<< Updated upstream
-from .models import Genre, Book
-=======
+
 from .models import Genre, Book, RentedBook
 from cds.models import CD, CDGenre, Song, RentedCD
 from films.models import Film, FilmGenre, RentedFilm
->>>>>>> Stashed changes
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Book)
-<<<<<<< Updated upstream
-=======
 admin.site.register(RentedBook)
 
 admin.site.register(CD)
@@ -22,4 +17,4 @@ admin.site.register(RentedCD)
 admin.site.register(Film)
 admin.site.register(FilmGenre)
 admin.site.register(RentedFilm)
->>>>>>> Stashed changes
+
--- robimy_django/settings.py
+++ robimy_django/settings.py
@@ -35,11 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< Updated upstream
-=======
     'cds',
     'films',
->>>>>>> Stashed changes
     'oldrental',
 ]
 
